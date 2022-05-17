b6dc1e7c1939a51c9c451c05599d41e36c486f4f
ranjankjha17 <talk2ranjanktr@gmail.com>
2022-05-17 14:39:57 +0530

    --

--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -7,7 +7,7 @@ import {providers, Contract} from "ethers";
 import { WHITELIST_CONTRACT_ADDRESS, abi } from "../constants";
 
 export default function Home() {
-	//
+	//17/5/22
   const [numberOfWhitelisted, setNumberOfWhitelisted] = useState(0);
 
   const [walletConnected,setWalletConnected]=useState(false);
